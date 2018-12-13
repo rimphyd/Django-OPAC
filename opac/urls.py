3406d17512614ea1a85bdf2533bb0692eeae536b
Ryuichi Sasaki <38239786+Ryuichi-Sasaki@users.noreply.github.com>
2018-12-13 17:32:26 +0900

    Feature/add book detail view (#32)
    * Add detail link to book list
    
    * Add BookDetailView
    
    * Add book detail template

--- opac/urls.py
+++ opac/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'opac'
 urlpatterns = [
     path('search/', views.SearchView.as_view(), name='search'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
 ]
--- opac/views/__init__.py
+++ opac/views/__init__.py
@@ -1,1 +1,2 @@
+from opac.views.book_detail import BookDetailView  # noqa: F401
 from opac.views.search import SearchView  # noqa: F401
