a3a1f44b3108d5695d85583f694194576cbc42d4
tomitake <tomitake.yokosekanemoto@gmail.com>
2018-12-19 00:58:41 +0900

    Add back action to LendingAdmin

--- opac/admin/messages/transactions/lending.py
+++ opac/admin/messages/transactions/lending.py
@@ -2,3 +2,4 @@ class LendingAdminMessage:
     ALREADY_RENEWED = '既に延長されているので延長できませんでした。'
     RESERVATION_EXISTS = '予約が入っているので延長できませんでした。'
     RENEWED = '選択された 貸出 を延長しました。'
+    BACKED = '選択された 貸出 を返却しました。'
