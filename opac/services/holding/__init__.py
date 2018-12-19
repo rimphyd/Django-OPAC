cfc4a71ab5a075a237305fdc2f89beef79610d9e
tomitake <tomitake.yokosekanemoto@gmail.com>
2018-12-20 00:32:42 +0900

    Add HoldingAdmin cancel action

--- opac/services/holding/__init__.py
+++ opac/services/holding/__init__.py
@@ -1,1 +1,2 @@
+from .cancel import *  # noqa: F401 F403
 from .lend import *  # noqa: F401 F403
---
+++ opac/services/holding/cancel.py
@@ -0,0 +1,31 @@
+from opac.queries import QueryError, HoldingCancelQuery
+from opac.services import ServiceError
+
+
+class HoldingCancelService:
+    """取置の取り消しを行うサービス。
+
+    Parameters
+    ----------
+    holding
+        対象の取置
+    """
+    def __init__(self, holding):
+        self._holding = holding
+
+    def exec(self):
+        """サービスを実行する。
+
+        Raises
+        ------
+        ServiceError
+            取り消し処理でエラーが発生した場合。
+        """
+        try:
+            holding_created = HoldingCancelQuery(self._holding).exec()
+            if holding_created:
+                # TODO メールを送る
+                pass
+        except QueryError as e:
+            # TODO ログを仕込む
+            raise ServiceError(repr(e))
