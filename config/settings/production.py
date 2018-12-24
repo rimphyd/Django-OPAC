753f4adb28f182197689e27b4083a7a9de045617
tomitake <tomitake.yokosekanemoto@gmail.com>
2018-12-24 17:42:42 +0900

    Add logging setting to production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,6 +12,36 @@ SECRET_KEY = os.environ['SECRET_KEY']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s %(module)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'opac': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
+
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
