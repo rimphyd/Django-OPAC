ac00bb0e11d49ba49ecc6c3161b2d7b2f52c7dac
tomitake <tomitake.yokosekanemoto@gmail.com>
2018-12-28 23:18:32 +0900

    Add login/logout redirect urls to settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -99,3 +99,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
+
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
